4f93d5b6afc508979346092749b01f59eea74389
MUHAMMED JAVITH ABDUL JAFFAR <muhammadjavith.a@gmail.com>
2024-12-31 14:21:47 +0530

    remove unwanted files

--- employer/src/main/java/com/mj/employer/exception/EmployerAlreadyExistException.java
+++
@@ -1,9 +0,0 @@
-package com.mj.employer.exception;
-
-public class EmployerAlreadyExistException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public EmployerAlreadyExistException(String message) {
-		super(message);
-	}
-}
--- employer/src/main/java/com/mj/employer/repository/EmployerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mj.employer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mj.employer.entity.Employer;
-
-public interface EmployerRepository extends JpaRepository<Employer, Long> {
-
-}
