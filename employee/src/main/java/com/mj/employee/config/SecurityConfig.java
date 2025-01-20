d5e42166348b097b3b10cc6b8bdffff0a417a873
MUHAMMED JAVITH ABDUL JAFFAR <muhammadjavith.a@gmail.com>
2025-01-20 17:20:06 +0530

    default spring security form is removed

--- employee/src/main/java/com/mj/employee/config/SecurityConfig.java
+++ employee/src/main/java/com/mj/employee/config/SecurityConfig.java
@@ -1,16 +1,19 @@
 package com.mj.employee.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 	@Bean
-	public SecurityFilterChain userDefinedFilter(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(request -> request.requestMatchers("/api/employeess/").permitAll()
-				.anyRequest().authenticated()).formLogin();
+	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+		http.apply(new HttpBasicConfigurer<>());
 		return http.build();
 	}
 
