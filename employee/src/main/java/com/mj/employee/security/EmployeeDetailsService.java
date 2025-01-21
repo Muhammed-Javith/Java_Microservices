9701303ff89a22abf33262c148f6af01e47f1040
MUHAMMED JAVITH ABDUL JAFFAR <muhammadjavith.a@gmail.com>
2025-01-21 13:52:26 +0530

    implemented secuirty with database verification

---
+++ employee/src/main/java/com/mj/employee/security/EmployeeDetailsService.java
@@ -0,0 +1,24 @@
+package com.mj.employee.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import com.mj.employee.enity.Employee;
+import com.mj.employee.exception.EmployeeNotFoundException;
+import com.mj.employee.repository.EmployeeRepository;
+
+@Service
+public class EmployeeDetailsService implements UserDetailsService {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws EmployeeNotFoundException {
+		Employee employee = employeeRepository.findByEmail(username)
+				.orElseThrow(() -> new EmployeeNotFoundException("Employee with email " + username + " not found"));
+		return new EmployeePrincipal(employee);
+	}
+}
---
+++ employee/src/main/java/com/mj/employee/security/EmployeePrincipal.java
@@ -0,0 +1,59 @@
+package com.mj.employee.security;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.mj.employee.enity.Employee;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeePrincipal implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	private Employee employee;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singleton(new SimpleGrantedAuthority("USER"));
+	}
+
+	@Override
+	public String getPassword() {
+		return employee.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return employee.getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
